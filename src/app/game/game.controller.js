dea5bb041035a3d78520316ea929847d67658414
zdlopez <zachary.d.lopez@gmail.com>
2015-01-19 16:03:41 -0800

    working on wall placement

--- src/app/game/game.controller.js
+++ src/app/game/game.controller.js
@@ -40,31 +40,26 @@ angular.module('appMaze')
 
           //right 
           wall = cell & 2;
-          //wall >> 1;
-          
 
           if(wall === 2){
-            //has top wall
+            //has right wall
             console.log('cell ', row, col, 'has a right wall');
           }
 
           //bottom
           wall = cell & 4;
-          //wall >> 1;
           
 
           if(wall === 4){
-            //has top wall
+            //has bottom wall
             console.log('cell ', row, col, 'has a bottom wall');
           }
 
           //left
           wall = cell & 8;
-          //wall >> 1;
-          
 
           if(wall === 8){
-            //has top wall
+            //has left wall
             console.log('cell ', row, col, 'has a right wall');
           }
 
@@ -72,7 +67,148 @@ angular.module('appMaze')
 
         }
       }
+    };
+
+    mazes.go = function(){
+      console.log('i am going');
+      mazes.threeHelper();
+      //mazes.animate();
     }
+
+    mazes.threeHelper = function()
+    {
+      var WIDTH = window.innerWidth,
+        HEIGHT = window.innerHeight,
+        ASPECT = WIDTH / HEIGHT,
+        UNITSIZE = 240,
+        WALLTHICKNESS = 20,
+        WALLHEIGHT = UNITSIZE / 3,
+        MOVESPEED = 100,
+        LOOKSPEED = 0.075,
+        NUMAI = 5;
+
+      var t = THREE,scene, cam, renderer, controls, clock, projector, model, skin;
+      var runAnim = true, mouse = { x: 0, y: 0 };
+
+
+      var init = function(){
+
+
+        clock = new t.Clock(); // A high-performance timer used to calculate the time between rendering frames in order to smooth animation
+        projector = new t.Projector(); // A helper class for projecting 2D rays (on the screen) into 3D rays (in the virtual world)
+        scene = new t.Scene(); // The "world" environment. Holds all other objects.
+        scene.fog = new t.FogExp2(0xD6F1FF, 0.0005); // Add fog to the world. Helps with depth perception. Params are color (in hex) and density
+       
+        // Set up camera so we know from where to render the scene
+        cam = new t.PerspectiveCamera(60, ASPECT, 1, 10000); // Field Of Viw, aspect ratio, near, far
+        cam.position.y = UNITSIZE * .2; // Raise the camera off the ground
+        scene.add(cam); // Add the camera to the scene
+       
+        // Camera moves with mouse, flies around with WASD/arrow keys
+        controls = new t.FirstPersonControls(cam); // Handles camera control
+        controls.movementSpeed = MOVESPEED; // How fast the player can walk around
+        controls.lookSpeed = LOOKSPEED; // How fast the player can look around with the mouse
+        controls.lookVertical = false; // Don't allow the player to look up or down. This is a temporary fix to keep people from flying
+        controls.noFly = true; // Don't allow hitting R or F to go up or down
+       
+        // World objects
+        setupScene(); // Adds physical objects to the world. Described later
+       
+        // Handle drawing as WebGL (faster than Canvas but less supported by browsers)
+        renderer = new t.WebGLRenderer();
+        renderer.setSize(WIDTH, HEIGHT); // Give the renderer the canvas size explicitly
+
+        // Add the canvas to the document
+        renderer.domElement.style.backgroundColor = '#D6F1FF'; // Make it easier to see that the canvas was added. Also this is the sky color
+        document.body.appendChild(renderer.domElement); // Add the canvas to the document
+       
+        // Track mouse position (set mouse.x and mouse.y to pointer coordinates) so we know where to shoot
+        document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+       
+
+     
+      }
+
+
+      // Set up the objects in the world
+      function setupScene() {
+        //var units = mapW;
+       
+        // Geometry: floor
+        var floor = new t.Mesh(
+            new t.CubeGeometry(mazes.n * UNITSIZE, 10, mazes.n * UNITSIZE),
+            new t.MeshLambertMaterial({color: 0xEDCBA0})
+        );
+        scene.add(floor);
+       
+        // Geometry: walls
+        var topBottomWall = new t.BoxGeometry(UNITSIZE, WALLHEIGHT, WALLTHICKNESS);
+        var rightLeftWall = new t.BoxGeometry(WALLTHICKNESS, WALLHEIGHT, UNITSIZE);
+        var materials = new t.MeshLambertMaterial({color: 0xff0000});
+        var tbWall = new t.Mesh(topBottomWall, materials);
+        var rlWall = new t.Mesh(rightLeftWall, meterials);
+        for (var row = 0; row < mazes.n; row++) {
+          for (var col = 0; col < mazes.n; col++) {
+            var cell = mazes.maze[row][col];
+            var wall;
+            
+            //top
+            wall = cell & 1;
+            
+
+            if(wall === 1){
+              //has top wall
+
+
+
+              console.log('cell ', row, col, 'has a top wall');
+            }
+
+            //right 
+            wall = cell & 2;
+
+            if(wall === 2){
+              //has right wall
+              console.log('cell ', row, col, 'has a right wall');
+            }
+
+            //bottom
+            wall = cell & 4;
+            
+
+            if(wall === 4){
+              //has bottom wall
+              console.log('cell ', row, col, 'has a bottom wall');
+            }
+
+            //left
+            wall = cell & 8;
+
+            if(wall === 8){
+              //has left wall
+              console.log('cell ', row, col, 'has a right wall');
+            }
+            // if (map[i][j]) {
+            //   var wall = new t.Mesh(cube, materials[map[i][j]-1]);
+            //   wall.position.x = (i - mazes.n/2) * UNITSIZE;
+            //   wall.position.y = WALLHEIGHT/2;
+            //   wall.position.z = (j - mazes.n/2) * UNITSIZE;
+            //   scene.add(wall);
+            // }
+          }
+        }
+
+        // Lighting
+          var directionalLight1 = new t.DirectionalLight( 0xF7EFBE, 0.7 );
+          directionalLight1.position.set( 0.5, 1, 0.5 );
+          scene.add( directionalLight1 );
+          var directionalLight2 = new t.DirectionalLight( 0xF7EFBE, 0.5 );
+          directionalLight2.position.set( -0.5, -1, -0.5 );
+          scene.add( directionalLight2 );
+        }
+
+    };
+
 
     return mazes;
 
